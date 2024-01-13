e75670f05e04ed7ecfcd321c075a8128150bb5ba
Parham Alvani <parham.alvani@gmail.com>
2024-01-13 10:15:32 +0000

    feat: include all required scripts into one

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+dotfile_lib_root=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# shellcheck source=message.sh
+source "$dotfile_lib_root/message.sh"
+# shellcheck source=proxy.sh
+source "$dotfile_lib_root/proxy.sh"
+# shellcheck source=linker.sh
+source "$dotfile_lib_root/linker.sh"
+# shellcheck source=require.sh
+source "$dotfile_lib_root/require.sh"
